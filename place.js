ece88ea65096e541b1544c85873d50b5476e518a
abrarzahed <abrarzahed6986@gmail.com>
2022-07-27 22:13:12 +0600

    added dynamic place name

--- place.js
+++ place.js
@@ -1,187 +1,67 @@
-
-
-// function initMap() {
-
-
-
-//     const map = new google.maps.Map(
-//         document.getElementById("map"),
-//         {
-//             center: { lat: 40.749933, lng: -73.98633 },
-//             zoom: 13,
-//             mapTypeControl: false,
-//         }
-//     );
-//     const card = document.getElementById("pac-card");
-//     const input = document.getElementById("pac-input");
-//     const biasInputElement = document.getElementById(
-//         "use-location-bias"
-//     );
-//     const strictBoundsInputElement = document.getElementById(
-//         "use-strict-bounds"
-//     );
-//     const options = {
-//         fields: ["formatted_address", "geometry", "name"],
-//         strictBounds: false,
-//         types: ["(regions)"],
-//     };
-
-//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
-
-//     const autocomplete = new google.maps.places.Autocomplete(input, options);
-
-//     input.addEventListener('input', () => {
-//         const container = document.querySelector('.pac-container')
-//         container.classList.remove('pac-logo')
-//         const innerDiv = document.createElement('div')
-//         innerDiv.classList.add('inner')
-
-
-//         container.append(innerDiv)
-//         console.log('pac-container', container)
-//         console.log('autocomplete', autocomplete);
-//     })
-
-
-
-//     // Bind the map's bounds (viewport) property to the autocomplete object,
-//     // so that the autocomplete requests use the current map bounds for the
-//     // bounds option in the request.
-//     autocomplete.bindTo("bounds", map);
-
-//     const infowindow = new google.maps.InfoWindow();
-//     const infowindowContent = document.getElementById(
-//         "infowindow-content"
-//     );
-
-//     infowindow.setContent(infowindowContent);
-
-//     console.log('infowindow', infowindow);
-
-
-
-//     const marker = new google.maps.Marker({
-//         map,
-//         anchorPoint: new google.maps.Point(0, -29),
-//     });
-
-
-
-//     autocomplete.addListener("place_changed", () => {
-//         infowindow.close();
-//         marker.setVisible(false);
-
-
-
-//         const place = autocomplete.getPlace();
-
-//         console.log('place', place);
-
-//         // const urlParams = new URLSearchParams(window.location.search);
-//         window.location.assign(`/area.html?place=${place.formatted_address}`)
-
-//         // urlParams.set('place', place.name);
-//         // window.location.search = urlParams;
-
-
-//         if (!place.geometry || !place.geometry.location) {
-//             // User entered the name of a Place that was not suggested and
-//             // pressed the Enter key, or the Place Details request failed.
-//             window.alert("No details available for input: '" + place.name + "'");
-//             return;
-//         }
-
-//         // If the place has a geometry, then present it on a map.
-//         if (place.geometry.viewport) {
-//             map.fitBounds(place.geometry.viewport);
-//         } else {
-//             map.setCenter(place.geometry.location);
-//             map.setZoom(17);
-//         }
-
-//         marker.setPosition(place.geometry.location);
-//         marker.setVisible(true);
-
-
-//     });
-
-//     // Sets a listener on a radio button to change the filter type on Places
-//     // Autocomplete.
-//     function setupClickListener(id, types) {
-//         const radioButton = document.getElementById(id);
-
-//     }
-
-//     setupClickListener("changetype-all", []);
-//     setupClickListener("changetype-address", ["address"]);
-//     setupClickListener("changetype-establishment", ["establishment"]);
-//     setupClickListener("changetype-geocode", ["geocode"]);
-//     setupClickListener("changetype-cities", ["(cities)"]);
-//     setupClickListener("changetype-regions", ["(regions)"]);
-
-
-
-
-// }
 function initMap() {
-    const resultDiv = document.querySelector('.search_area--result')
-    const btnSearch = document.querySelector('.btn-search')
+  const resultDiv = document.querySelector(".search_area--result");
+  const btnSearch = document.querySelector(".btn-search");
 
-    const input = document.getElementById("pac-input");
+  const resultAreaName = document.querySelector(".result-area-name");
 
-    const options = {
-        fields: ["formatted_address", "geometry", "name"],
-        strictBounds: false,
-        types: ["(regions)"],
-    };
+  const input = document.getElementById("pac-input");
 
-    const autocomplete = new google.maps.places.Autocomplete(input, options);
+  const options = {
+    fields: ["formatted_address", "geometry", "name"],
+    strictBounds: false,
+    types: ["(regions)"],
+  };
 
-    input.addEventListener('input', () => {
-        const container = document.querySelector('.pac-container')
-        container.classList.remove('pac-logo')
-        const innerDiv = document.createElement('div')
-        innerDiv.classList.add('inner')
+  const autocomplete = new google.maps.places.Autocomplete(input, options);
 
-        if (input.value.length == 0) {
-            resultDiv.classList.add('hidden')
-        }
+  input.addEventListener("input", () => {
+    const container = document.querySelector(".pac-container");
+    container.classList.remove("pac-logo");
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("inner");
 
-        container.append(innerDiv)
-        console.log('pac-container', container)
-        console.log('autocomplete', autocomplete);
-    })
+    if (input.value.length == 0) {
+      resultDiv.classList.add("hidden");
+    }
 
-    btnSearch.addEventListener('click', function () {
-        if (input.value.length > 0) {
-            resultDiv.classList.remove('hidden')
-        }
-    })
+    container.append(innerDiv);
+    console.log("pac-container", container);
+    console.log("autocomplete", autocomplete);
+  });
 
+  //=== show result  ===//
+  const showResult = function (place) {
+    resultAreaName.textContent = place.formatted_address;
+  };
 
-    autocomplete.addListener("place_changed", () => {
-        resultDiv.classList.remove('hidden')
+  btnSearch.addEventListener("click", function () {
+    if (input.value.length > 0) {
+      const place = autocomplete.getPlace();
+      showResult(place);
+      resultDiv.classList.remove("hidden");
+    }
+  });
 
-        const place = autocomplete.getPlace();
-        console.log('place', place);
+  autocomplete.addListener("place_changed", () => {
+    resultDiv.classList.remove("hidden");
 
-        // const urlParams = new URLSearchParams(window.location.search);
-        // window.location.assign(`/area.html?place=${place.formatted_address}`)
+    const place = autocomplete.getPlace();
+    showResult(place);
+    // console.log("place", place);
 
-        // urlParams.set('place', place.name);
-        // window.location.search = urlParams;
+    // const urlParams = new URLSearchParams(window.location.search);
+    // window.location.assign(`/area.html?place=${place.formatted_address}`)
 
-        if (!place.geometry || !place.geometry.location) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-        }
-    });
+    // urlParams.set('place', place.name);
+    // window.location.search = urlParams;
 
+    if (!place.geometry || !place.geometry.location) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+  });
 }
 
 window.initMap = initMap;
-
-
-
