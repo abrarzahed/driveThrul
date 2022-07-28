f6df81c0278b31bf61f93c3a4d3e42a57c14fea7
abrarzahed <abrarzahed6986@gmail.com>
2022-07-28 20:20:57 +0600

    added manual and autometic price dynamic data

--- place.js
+++ place.js
@@ -1,11 +1,11 @@
 function initMap() {
   //=== dom selection  ===//
   const resultDiv = document.querySelector(".search_area--result");
-  const btnSearch = document.querySelector(".btn-search");
+  // const btnSearch = document.querySelector(".btn-search");
   const input = document.getElementById("pac-input");
   const resultAreaName = document.querySelector(".result-area-name");
-  const resultAreaPrice = document.querySelector(".result-area-price");
-  const resultAreaInfo = document.querySelector(".result-area-info");
+  const priceCardsManual = document.querySelector(".manual-cards");
+  const priceCardsAuto = document.querySelector(".auto-cards");
 
   const options = {
     fields: ["formatted_address", "geometry", "name"],
@@ -20,18 +20,48 @@ function initMap() {
       areaName: "sw1a",
       price: "$120 for SW1a",
       info: "5 HOURS BLOCK BOOKING",
+      auto: [
+        { hour: 2, price: "$70" },
+        { hour: 4, price: "$90" },
+        { hour: 6, price: "$110" },
+      ],
+      manual: [
+        { hour: 2, price: "$110" },
+        { hour: 4, price: "$150" },
+        { hour: 6, price: "$190" },
+      ],
     },
     {
       formattedAddress: "Southall UB1, UK",
       areaName: "ub1",
       price: "$220 for UB1",
       info: "15 HOURS BLOCK BOOKING",
+      auto: [
+        { hour: 2, price: "$71" },
+        { hour: 4, price: "$91" },
+        { hour: 6, price: "$111" },
+      ],
+      manual: [
+        { hour: 2, price: "$112" },
+        { hour: 4, price: "$151" },
+        { hour: 6, price: "$191" },
+      ],
     },
     {
       formattedAddress: "Plymouth PL5, UK",
       areaName: "pl5",
       price: "$320 for PL5",
       info: "55 HOURS BLOCK BOOKING",
+      auto: [
+        { hour: 2, price: "$72" },
+        { hour: 4, price: "$92" },
+        { hour: 6, price: "$113" },
+      ],
+      manual: [
+        { hour: 2, price: "$117" },
+        { hour: 4, price: "$155" },
+        { hour: 6, price: "$199" },
+      ],
     },
   ];
 
@@ -53,16 +83,38 @@ function initMap() {
   //=== show result  ===//
   const showResult = function (area, isAreaAvailable) {
     resultDiv.classList.remove("hidden");
+
     if (isAreaAvailable) {
       resultAreaName.textContent = area.formattedAddress;
-      resultAreaPrice.textContent = area.price;
-      resultAreaInfo.textContent = area.info;
+      priceCardsManual.innerHTML = "";
+      priceCardsAuto.innerHTML = "";
+
+      area.manual.forEach((area) => {
+        priceCardsManual.innerHTML += `
+        <div class="gearbox-card">
+        <span>${area.hour}</span>
+        <span>Hours</span>
+        <span>${area.price}</span>
+        <a href="#" class="gearbox-link">BOOK</a>
+        </div>
+        `;
+      });
+
+      area.auto.forEach((area) => {
+        priceCardsAuto.innerHTML += `
+        <div class="gearbox-card">
+        <span>${area.hour}</span>
+        <span>Hours</span>
+        <span>${area.price}</span>
+        <a href="#" class="gearbox-link">BOOK</a>
+        </div>
+        `;
+      });
+
       resultDiv.classList.remove("not-found");
     } else {
       resultDiv.classList.add("not-found");
-      resultAreaName.textContent = "";
-      resultAreaPrice.textContent = "";
-      resultAreaInfo.textContent = "Sorry we do not cover this area yet";
+      resultAreaName.textContent = "Sorry we do not cover this area yet";
     }
   };
 
@@ -76,7 +128,7 @@ function initMap() {
     } else {
       showResult(selectedArea, true);
     }
-    console.log("selected area", selectedArea);
+    // console.log("selected area", selectedArea);
   };
 
   //=== select area by clicking on button  ===//
