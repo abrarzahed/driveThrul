d4184c65f9730ebf7aa845e966f7c5fbbb3c1378
abrarzahed <abrarzahed6986@gmail.com>
2022-07-28 18:05:48 +0600

    added test area data to check filter options

--- place.js
+++ place.js
@@ -1,10 +1,11 @@
 function initMap() {
+  //=== dom selection  ===//
   const resultDiv = document.querySelector(".search_area--result");
   const btnSearch = document.querySelector(".btn-search");
-
-  const resultAreaName = document.querySelector(".result-area-name");
-
   const input = document.getElementById("pac-input");
+  const resultAreaName = document.querySelector(".result-area-name");
+  const resultAreaPrice = document.querySelector(".result-area-price");
+  const resultAreaInfo = document.querySelector(".result-area-info");
 
   const options = {
     fields: ["formatted_address", "geometry", "name"],
@@ -12,45 +13,94 @@ function initMap() {
     types: ["(regions)"],
   };
 
+  //=== data  ===//
+  const postCodeAreaData = [
+    {
+      formattedAddress: "London SW1A, UK",
+      areaName: "sw1a",
+      price: "$120 for SW1a",
+      info: "5 HOURS BLOCK BOOKING",
+    },
+    {
+      formattedAddress: "Southall UB1, UK",
+      areaName: "ub1",
+      price: "$220 for UB1",
+      info: "15 HOURS BLOCK BOOKING",
+    },
+    {
+      formattedAddress: "Plymouth PL5, UK",
+      areaName: "pl5",
+      price: "$320 for PL5",
+      info: "55 HOURS BLOCK BOOKING",
+    },
+  ];
+
+  let selectedArea = {};
+
   const autocomplete = new google.maps.places.Autocomplete(input, options);
 
   input.addEventListener("input", () => {
+    //=== remove google brand   ===//
     const container = document.querySelector(".pac-container");
     container.classList.remove("pac-logo");
 
     if (input.value.length == 0) {
       resultDiv.classList.add("hidden");
+      // selectedArea = {};
     }
-
-    console.log("pac-container", container);
-    console.log("autocomplete", autocomplete);
   });
 
   //=== show result  ===//
-  const showResult = function (place) {
-    resultAreaName.textContent = place.formatted_address;
+  const showResult = function (area, isAreaAvailable) {
+    resultDiv.classList.remove("hidden");
+    if (isAreaAvailable) {
+      resultAreaName.textContent = area.formattedAddress;
+      resultAreaPrice.textContent = area.price;
+      resultAreaInfo.textContent = area.info;
+      resultDiv.classList.remove("not-found");
+    } else {
+      resultDiv.classList.add("not-found");
+      resultAreaName.textContent = "";
+      resultAreaPrice.textContent = "";
+      resultAreaInfo.textContent = "Sorry we do not cover this area yet";
+    }
   };
 
+  //=== select area  ===//
+  const selectArea = function (place) {
+    selectedArea = postCodeAreaData.find(
+      (item) => item.areaName == place.name.toLowerCase()
+    );
+    if (!selectedArea) {
+      showResult(selectedArea, false);
+    } else {
+      showResult(selectedArea, true);
+    }
+    console.log("selected area", selectedArea);
+  };
+
+  //=== select area by clicking on button  ===//
+  /*
   btnSearch.addEventListener("click", function () {
     if (input.value.length > 0) {
       const place = autocomplete.getPlace();
-      showResult(place);
-      resultDiv.classList.remove("hidden");
+      selectArea(place);
     }
   });
+  */
 
   autocomplete.addListener("place_changed", () => {
-    resultDiv.classList.remove("hidden");
-
     const place = autocomplete.getPlace();
-    showResult(place);
+    selectArea(place);
     // console.log("place", place);
 
-    // const urlParams = new URLSearchParams(window.location.search);
-    // window.location.assign(`/area.html?place=${place.formatted_address}`)
-
-    // urlParams.set('place', place.name);
-    // window.location.search = urlParams;
+    //=== redirect  ===//
+    /*
+    const urlParams = new URLSearchParams(window.location.search);
+    window.location.assign(`/area.html?place=${place.formatted_address}`)
+    urlParams.set('place', place.name);
+    window.location.search = urlParams;
+    */
 
     if (!place.geometry || !place.geometry.location) {
       // User entered the name of a Place that was not suggested and
